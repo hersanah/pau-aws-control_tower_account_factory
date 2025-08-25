153d4ea8f9b19958566aef3493421e55576ee526
hersanah <hadeyanju1804@gmail.com>
2025-08-25 15:14:12 +0100

    Variables Update - Remove terraform declarations from main.tf

--- modules/aft-feature-options/s3.tf
+++ modules/aft-feature-options/s3.tf
@@ -44,6 +44,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "aft_logging_bucket_lifecycle_c
     status = "Enabled"
     id     = "aft_logging_bucket_lifecycle_configuration_rule"
 
+    filter {
+      prefix = "AWSLogs/"
+    }
+
     noncurrent_version_expiration {
       noncurrent_days = var.log_archive_bucket_object_expiration_days
     }
